8b92255ecb0d424179621225b0dfedb1943aed6d
Michael Bayne <mdb@samskivert.com>
2020-04-22 12:30:09 -0700

    Indent hackery.
    Indent `where` under constructor decl & hack in non-indentation of namespaces.
    I need to make that configurable but no time right now.

--- src/scala/scaled/csharp/CSharpMode.scala
+++ src/scala/scaled/csharp/CSharpMode.scala
@@ -5,7 +5,7 @@
 package scaled.csharp
 
 import scaled._
-import scaled.code.{Commenter, BlockIndenter}
+import scaled.code.{Commenter, Indenter, BlockIndenter}
 import scaled.grammar.GrammarCodeMode
 
 @Major(name="csharp",
@@ -16,16 +16,7 @@ class CSharpMode (env :Env) extends GrammarCodeMode(env) {
 
   override def langScope = "source.cs"
 
-  override protected def createIndenter = new BlockIndenter(config, Std.seq(
-    // bump extends/implements in two indentation levels
-    BlockIndenter.adjustIndentWhenMatchStart(Matcher.regexp("(extends|implements)\\b"), 2),
-    // align changed method calls under their dot
-    new BlockIndenter.AlignUnderDotRule(),
-    // handle indenting switch statements properly
-    new BlockIndenter.SwitchRule(),
-    // handle continued statements, with some special sauce for : after case
-    new BlockIndenter.CLikeContStmtRule()
-  ))
+  override protected def createIndenter = new CSharpIndenter(config)
 
   override val commenter = new Commenter() {
     override def linePrefix  = "//"
@@ -35,6 +26,36 @@ class CSharpMode (env :Env) extends GrammarCodeMode(env) {
     override def docOpen     = "///"
     override def docPrefix   = "///"
   }
+}
 
-  // TODO: more things!
+class CSharpIndenter (config :Config) extends BlockIndenter(config, Std.seq(
+  // bump `extends`/`implements` in two indentation levels
+  BlockIndenter.adjustIndentWhenMatchStart(Matcher.regexp("(extends|implements)\\b"), 2),
+  // bump `where` in two indentation levels
+  BlockIndenter.adjustIndentWhenMatchStart(Matcher.regexp("(where)\\b"), 1),
+  // align changed method calls under their dot
+  new BlockIndenter.AlignUnderDotRule(),
+  // handle indenting switch statements properly
+  new BlockIndenter.SwitchRule(),
+  // handle continued statements, with some special sauce for : after case
+  new BlockIndenter.CLikeContStmtRule()
+)) {
+  import Indenter._
+  import BlockIndenter._
+
+  override protected def openBlock (line :LineV, open :Char, close :Char, col :Int, state :State) =
+    super.openBlock(line, open, close, col, state) match {
+      case bstate :BlockIndenter.BlockS if (col >= lastNonWS(line)) =>
+        if (line.matches(namespaceM)) new I0BlockS(close, -1, bstate.next)
+        else bstate
+      case ostate => ostate
+    }
+
+  private class I0BlockS (close :Char, col :Int, next :Indenter.State)
+      extends BlockS(close, col, next) {
+    override protected def indentWidth (config :Config) :Int = 0
+    override protected def show :String = s"I0BlockS($close, $col)"
+  }
+
+  private val namespaceM = Matcher.regexp("\\s*namespace ")
 }
