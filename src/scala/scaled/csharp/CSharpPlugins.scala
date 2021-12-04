0a6d8f33a8811f33210ff12412bb18f9361059f3
Michael Bayne <mdb@samskivert.com>
2021-12-04 11:59:15 -0800

    "Parse" the .sln and look for .csproj files.
    Really we need to parse those .csproj files to find further project
    dependencies to properly figure out source dirs for all the dependent projects,
    but this is better than nothing for now.

--- src/scala/scaled/csharp/CSharpPlugins.scala
+++ src/scala/scaled/csharp/CSharpPlugins.scala
@@ -5,6 +5,8 @@
 package scaled.project
 
 import java.nio.file.{Files, Path}
+import scala.jdk.CollectionConverters._
+import scala.util.matching._
 import scaled._
 
 /** Plugins to extract project metadata from `.sln` files. */
@@ -25,40 +27,53 @@ object CSharpPlugins {
 
     def addComponents (project :Project) :Unit = {
       val rootPath = project.root.path
-      findSln(rootPath).foreach { sln =>
-        val projName = rootPath.getFileName.toString // TOOD: read from sln?
+      val projName = rootPath.getFileName.toString // TOOD: read from sln?
+      val ignores = Ignorer.stockIgnores
+      ignores += Ignorer.ignoreName("bin")
+      ignores += Ignorer.ignoreName("obj")
+      // TODO: only ignore these if we detect Unity
+      ignores += Ignorer.ignoreName("bin~")
+      ignores += Ignorer.ignoreName("obj~")
+      ignores += Ignorer.ignoreRegex(".*\\.meta")
 
-        val sb = Ignorer.stockIgnores
-        sb += Ignorer.ignoreName("bin")
-        sb += Ignorer.ignoreName("obj")
+      // TODO: we should probably do this in stockIgnores?
+      ignores ++= Ignorer.gitIgnores(rootPath)
 
+      val sources = SeqBuffer[Path]()
+      def addProject (pdir :Path) :Unit = {
         // if this appears to be a Unity project, ignore Unity stuff as well
-        if (Files.exists(rootPath.resolve("Assets"))) {
-          sb += Ignorer.ignoreName("bin~")
-          sb += Ignorer.ignoreName("obj~")
-          sb += Ignorer.ignoreName("Library")
-          sb += Ignorer.ignoreName("Temp")
-
-          // add a sources component with our source directories
-          val sources = new Sources(
-            Seq(rootPath.resolve("Assets").resolve("Scripts"))
-          )
-          project.addComponent(classOf[Sources], sources)
-
+        if (Files.exists(pdir.resolve("Assets"))) {
+          ignores += Ignorer.ignorePath(pdir.resolve("Library"), rootPath)
+          ignores += Ignorer.ignorePath(pdir.resolve("Temp"), rootPath)
+          ignores += Ignorer.ignorePath(pdir.resolve("Logs"), rootPath)
         }
 
-        project.addComponent(classOf[Filer], new DirectoryFiler(project, sb))
-
-        // add a compiler that runs 'dotnet build' and parses the output
-        project.addComponent(classOf[Compiler], new DotNetCompiler(project, sln))
-
-        // // TODO: bower doesn't define source directories, so we hack some stuff
-        // val sourceDirs = Seq("src", "test").map(rootPath.resolve(_))
-        // project.addComponent(classOf[Sources], new Sources(sourceDirs))
-
-        val oldMeta = project.metaV()
-        project.metaV() = oldMeta.copy(name = projName)
+        sources += pdir
       }
+
+      for (sln <- findSln(rootPath) ;
+           slnLine <- Files.newBufferedReader(sln).lines.iterator.asScala) slnLine match {
+        case projRe(proj, path) if (path endsWith ".csproj") =>
+          val csproj = rootPath.resolve(path.replace('\\', '/'))
+          if (Files.exists(csproj)) addProject(csproj.getParent)
+          else println("Invalid .csproj file? " + csproj)
+          // case projRe(proj, path) => println("NOPE " + proj + " // " + path)
+        case line if (line startsWith "Project") => println("NOMATCH " + line)
+        case _ => // ignore
+      }
+
+      // add a sources component with our source directories
+      project.addComponent(classOf[Sources], new Sources(sources.toSeq))
+
+      project.addComponent(classOf[Filer], new DirectoryFiler(project, ignores))
+
+      // add a compiler that runs 'dotnet build' and parses the output
+      // project.addComponent(classOf[Compiler], new DotNetCompiler(project, sln))
+
+      val oldMeta = project.metaV()
+      project.metaV() = oldMeta.copy(name = projName)
     }
   }
+
+  val projRe = raw"""Project\("\{.*\}"\) = "(.*)", "(.*)", "\{.*\}"""".r
 }
